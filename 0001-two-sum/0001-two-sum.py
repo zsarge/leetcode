98555cbdfd32b4938fa872f1000e383ce4e25797
Zack Sargent <46602241+zsarge@users.noreply.github.com>
2023-10-10 17:27:12 -0400

    Time: 57 ms (92.28%), Space: 16.9 MB (99.60%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for x, a in enumerate(nums):
-            for y, b in enumerate(nums):
-                if x != y and a + b == target:
-                    return [x, y]
+        copy = sorted(nums)
+        start = 0
+        end = len(nums)-1
+        while start < end:
+            result = copy[start] + copy[end]
+            if result == target:
+                lastIndex = len(nums) -nums[::-1].index(copy[end])-1
+                return [nums.index(copy[start]), lastIndex]
+            elif result < target:
+                start += 1
+            elif result > target:
+                end -= 1
         raise Exception(f"Invalid Input {nums}")
