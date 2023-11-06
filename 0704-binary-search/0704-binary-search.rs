99fe1bd500c6d5064f3dd93aa7e2ab382f5dbf01
Zack Sargent <46602241+zsarge@users.noreply.github.com>
2023-11-06 11:51:10 -0500

    Time: 4 ms (80.33%), Space: 2.1 MB (63.47%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut low = 0;
+        let mut high = nums.len()-1;
+        while low <= high {
+            let midpoint = (low+high)/2;
+            if nums[midpoint] == target {
+                return midpoint as i32; // convert from usize
+            } else if target < nums[midpoint] {
+                if midpoint == 0 {
+                    break;
+                }
+                high = midpoint - 1;
+            } else { // target > nums[midpoint]
+                low = midpoint + 1;
+            }
+        }
+        return -1;
+    }
+}
