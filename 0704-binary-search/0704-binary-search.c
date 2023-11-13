326757e82371b056b2c8422eadfb108452979402
Zack Sargent <46602241+zsarge@users.noreply.github.com>
2023-11-13 16:26:51 -0500

    Time: 30 ms (36.84%), Space: 7.8 MB (31.81%) - LeetHub

--- 0704-binary-search/0704-binary-search.c
+++ 0704-binary-search/0704-binary-search.c
@@ -1,15 +1,14 @@
-// assume a sorted array
 int search(int* nums, int numsSize, int target) {
-    int *i = nums;
-    int *j = nums + (numsSize-1);
-    while (i <= j) {
-        int *midpoint = i + ((int)(j-i) / 2);
-        if (*midpoint == target) {
-            return (int)(midpoint-nums);
-        } else if (*midpoint > target) {
-            j = midpoint - 1;
-        } else { // *midpoint < target
-            i = midpoint + 1;
+    int i = 0;
+    int j = numsSize - 1;
+    while (i <= j){
+        int mid = i + ((int)(j - i) / 2);
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            i = mid + 1;
+        } else { // nums[mid] > target
+            j = mid - 1;
         }
     }
     return -1;
